086060622c3792265dc5186aa0924704d5544362
matheus mozart <mmsnborges@gmail.com>
2024-08-12 19:34:00 -0300

    botton component

---
+++ corelab-fron-tend/src/components/button/Button.tsx
@@ -0,0 +1,20 @@
+import { ButtonHTMLAttributes } from 'react';
+import styles from './button.module.scss';
+
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  loading?: boolean;
+  prefix?: any;
+  suffix?: any;
+}
+
+export function Button({ className, loading, text, prefix, suffix, ...props }: IButtonProps) {
+  return (
+    <button {...props} className={styles.button__component}>
+      {loading && <span className=""></span>}
+      {prefix && prefix}
+      {text}
+      {suffix && suffix}
+    </button>
+  );
+}
--- corelab-fron-tend/src/pages/home.tsx
+++ corelab-fron-tend/src/pages/home.tsx
@@ -1,17 +1,38 @@
+import { Button } from '../components/button/Button';
 import styles from './index.module.scss';
 
 export default function HomeScreen() {
   return (
-    <section className={styles.main__container}>
-      <section className={styles.main__container__content}>
-        <h1>Gerencie suas atividades diárias</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi commodi iste nemo nulla?
-          Amet quo quibusdam, dolore sit illo obcaecati ratione quam asperiores quis consequatur
-          aspernatur id distinctio blanditiis. Quasi!
-        </p>
-        <a href="">Acessar</a>
+    <>
+      <section className={styles.main__container}>
+        <section className={styles.main__container__content}>
+          <h1>Gerencie suas atividades diárias</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi commodi iste nemo nulla?
+            Amet quo quibusdam, dolore sit illo obcaecati ratione quam asperiores quis consequatur
+            aspernatur id distinctio blanditiis. Quasi!
+          </p>
+          <Button text={'Saiba mais'} />
+        </section>
+        <section></section>
       </section>
-    </section>
+
+      <section className={styles.about__container}>
+        <div className={styles.about__container__content}>
+          <div className={styles.about__container__content__text}>
+            <h2>Algo importante</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni blanditiis quas optio
+              ad accusamus, cupiditate itaque ex, dignissimos vero sapiente suscipit magnam incidunt
+              accusantium libero quia nam inventore iste excepturi.
+            </p>
+            <Button text={'Saiba mais'} />
+          </div>
+        </div>
+        <div className={styles.about__container__content}>
+          <img src="/sobre.png" alt="sobre_nos_tasks" />
+        </div>
+      </section>
+    </>
   );
 }
